e66d67417648f7f5ba93dc4ebe39f02c62e90c62
Wah0airo <luis.valdivia@stud.sbg.ac.at>
2016-03-14 20:57:46 +0100

    Assignment 1

--- selfie.c
+++ selfie.c
@@ -625,6 +625,10 @@ int OP_SW      = 43;
 int *OPCODES; // array of strings representing MIPS opcodes
 
 int FCT_NOP     = 0;
+int FCT_SLL     = 0;
+int FCT_SRL     = 2;
+int FCT_SLLV    = 4;
+int FCT_SRLV    = 6;
 int FCT_JR      = 8;
 int FCT_SYSCALL = 12;
 int FCT_MFHI    = 16;
@@ -664,6 +668,10 @@ void initDecoder() {
     FUNCTIONS = malloc(43 * SIZEOFINTSTAR);
 
     *(FUNCTIONS + FCT_NOP)     = (int) "nop";
+    *(FUNCTIONS + FCT_SLL)     = (int) "sll";
+    *(FUNCTIONS + FCT_SRL)     = (int) "srl";
+    *(FUNCTIONS + FCT_SLLV)    = (int) "sllv";
+    *(FUNCTIONS + FCT_SRLV)    = (int) "srlv";
     *(FUNCTIONS + FCT_JR)      = (int) "jr";
     *(FUNCTIONS + FCT_SYSCALL) = (int) "syscall";
     *(FUNCTIONS + FCT_MFHI)    = (int) "mfhi";
@@ -887,6 +895,10 @@ void fct_multu();
 void fct_divu();
 void fct_addu();
 void fct_subu();
+void fct_sll();
+void fct_sllv();
+void fct_slr();
+void fct_slrv();
 void op_lw();
 void fct_slt();
 void op_sw();
@@ -5447,6 +5459,155 @@ void fct_subu() {
     }
 }
 
+void fct_sllv() {
+    if(debug) {
+        printFunction(function);
+        print((int*) " ");
+        printRegister(rd);
+        print((int*) ",");
+        printRegister(rt);
+        print((int*) ",");
+        printRegister(rs);
+        if(interpret) {
+            print((int*) ": ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rt);
+            print((int*) "=");
+            print(itoa(*(registers+rt), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rs);
+            print((int*) "=");
+            print(itoa(*(registers+rs), string_buffer, 10, 0, 0));
+        }
+    }
+    if(interpret) {
+        *(registers+rd) = leftShift(registers+rt, registers+rs);
+        pc = pc+ WORDSIZE;
+    }
+    if (debug) {
+        if (interpret) {
+            print((int*) " -> ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+        }
+        println();
+    }
+}
+
+void fct_slrv() {
+    if(debug) {
+        printFunction(function);
+        print((int*) " ");
+        printRegister(rd);
+        print((int*) ",");
+        printRegister(rt);
+        print((int*) ",");
+        printRegister(rs);
+        if(interpret) {
+            print((int*) ": ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rt);
+            print((int*) "=");
+            print(itoa(*(registers+rt), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rs);
+            print((int*) "=");
+            print(itoa(*(registers+rs), string_buffer, 10, 0, 0));
+        }
+    }
+    if(interpret) {
+        *(registers+rd) = rightShift(registers+rt, registers+rs);
+        pc = pc+ WORDSIZE;
+    }
+    if (debug) {
+        if (interpret) {
+            print((int*) " -> ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+        }
+        println();
+    }
+}
+
+void fct_sll() {
+    if (debug) {
+        printOpcode(opcode);
+        print((int*) " ");
+        printRegister(rd);
+        print((int*) ",");
+        printRegister(rt);
+        print((int*) ",");
+        print(itoa(signExtend(immediate), string_buffer, 10, 0, 0));
+        if (interpret) {
+            print((int*) ": ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rt);
+            print((int*) "=");
+            print(itoa(*(registers+rt), string_buffer, 10, 0, 0));
+        }
+    }
+    if (interpret) {
+        *(registers+rd) = leftShift(registers+rt, signExtend(immediate));
+        pc = pc + WORDSIZE;
+    }
+    if (debug) {
+        if (interpret) {
+            print((int*) " -> ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+        }
+        println();
+    }
+}
+
+
+void fct_slr() {
+    if (debug) {
+        printOpcode(opcode);
+        print((int*) " ");
+        printRegister(rd);
+        print((int*) ",");
+        printRegister(rt);
+        print((int*) ",");
+        print(itoa(signExtend(immediate), string_buffer, 10, 0, 0));
+        if (interpret) {
+            print((int*) ": ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+            print((int*) ",");
+            printRegister(rt);
+            print((int*) "=");
+            print(itoa(*(registers+rt), string_buffer, 10, 0, 0));
+        }
+    }
+    if (interpret) {
+        *(registers+rd) = rightShift(registers+rt, signExtend(immediate));
+        pc = pc + WORDSIZE;
+    }
+    if (debug) {
+        if (interpret) {
+            print((int*) " -> ");
+            printRegister(rd);
+            print((int*) "=");
+            print(itoa(*(registers+rd), string_buffer, 10, 0, 0));
+        }
+        println();
+    }
+}
+
 void op_lw() {
     int vaddr;
 
@@ -6421,8 +6582,7 @@ int main(int argc, int *argv) {
 
     argc = argc - 1;
     argv = argv + 1;
-    //Test3
-    print((int *) "This is Oohoo3ic Selfie");
+    print((int*) "This is Oohoo3ic Selfie");
     println();
     if (selfie(argc, (int*) argv) != 0) {
         print(selfieName);
